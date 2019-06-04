0d5982059d8f4799c8047f7794724df0be8eac66
Emi Brown <emicb55@gmail.com>
2019-06-04 18:10:04 -0500

    Initial commit

---
+++ Assets/Scripts/AssetBundleStuff/Editor/BuildScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class BuildScript : Editor {
+    //creates menu option under assets to build assetbundles
+    [MenuItem("Assets/Build AssetBundles")]
+    //builds assetbundle
+    static void BuildAllAssetBundles() {
+        //location to build asset bundle
+        string assetBundleDirectory = "/Users/emicb/Desktop/TestBundles";
+        //creates directory if it does not exist already
+        if (!Directory.Exists(assetBundleDirectory)) {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        //builds to directory, uses LZMA compression & LZ4 recompression, builds to user's build target
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
---
+++ Assets/Scripts/AssetBundleStuff/LoadAssetBundles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadAssetBundles : MonoBehaviour {
+    string assetBundleServerURL = "https://www.dropbox.com/sh/n3frhilnpkozbmu/AABwkFnjxgwUJcQ0zSWuhnpJa/monkeys.unity3d?dl=1";
+    UnityWebRequest webRequest;
+    AssetBundle ab;
+
+    void Start() {
+        StartCoroutine(GetAssetBundle());
+    }
+
+    //coroutine to get assetbundle from webserver
+    IEnumerator GetAssetBundle() {
+        //web request to get assetbundle from webserver, skips crc
+        webRequest = UnityWebRequestAssetBundle.GetAssetBundle(assetBundleServerURL);
+        Debug.Log(webRequest == null ? "No web request" : "Web request exists");
+        yield return webRequest.SendWebRequest();
+        //loads asset bundle
+        ab = DownloadHandlerAssetBundle.GetContent(webRequest);
+        Debug.Log(ab == null ? "Failed to load AssetBundle" : "Successfully Loaded AssetBundle");
+        //instantiate object from assetbundle
+        GameObject cube = ab.LoadAsset<GameObject>("monkey_lp");
+    }
+}
