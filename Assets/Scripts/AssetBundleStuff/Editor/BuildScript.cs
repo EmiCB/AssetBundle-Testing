881fed787ed234a06c26a26b51902373f44de27f
Emi Brown <emicb55@gmail.com>
2019-06-05 16:36:10 -0500

    Works!!!

--- Assets/Scripts/AssetBundleStuff/Editor/BuildScript.cs
+++ Assets/Scripts/AssetBundleStuff/Editor/BuildScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using System.IO;
+using UnityEngine;
 
 public class BuildScript : Editor {
     //creates menu option under assets to build assetbundles
@@ -7,10 +8,11 @@ public class BuildScript : Editor {
     //builds assetbundle
     static void BuildAllAssetBundles() {
         //location to build asset bundle
-        string assetBundleDirectory = "/Users/emicb/Desktop/TestBundles";
+        string assetBundleDirectory = "/Users/emicb/Desktop/Bundle";
         //creates directory if it does not exist already
         if (!Directory.Exists(assetBundleDirectory)) {
             Directory.CreateDirectory(assetBundleDirectory);
+            Debug.Log("works");
         }
         //builds to directory, uses LZMA compression & LZ4 recompression, builds to user's build target
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
--- Assets/Scripts/AssetBundleStuff/LoadAssetBundles.cs
+++ Assets/Scripts/AssetBundleStuff/LoadAssetBundles.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class LoadAssetBundles : MonoBehaviour {
-    string assetBundleServerURL = "https://www.dropbox.com/sh/n3frhilnpkozbmu/AABwkFnjxgwUJcQ0zSWuhnpJa/monkeys.unity3d?dl=1";
+    string assetBundleServerURL = "https://emicb.github.io/monkeys";
     UnityWebRequest webRequest;
     AssetBundle ab;
 
@@ -14,13 +14,14 @@ public class LoadAssetBundles : MonoBehaviour {
     //coroutine to get assetbundle from webserver
     IEnumerator GetAssetBundle() {
         //web request to get assetbundle from webserver, skips crc
-        webRequest = UnityWebRequestAssetBundle.GetAssetBundle(assetBundleServerURL);
+        webRequest = UnityWebRequestAssetBundle.GetAssetBundle(assetBundleServerURL, 0);
         Debug.Log(webRequest == null ? "No web request" : "Web request exists");
         yield return webRequest.SendWebRequest();
         //loads asset bundle
         ab = DownloadHandlerAssetBundle.GetContent(webRequest);
         Debug.Log(ab == null ? "Failed to load AssetBundle" : "Successfully Loaded AssetBundle");
         //instantiate object from assetbundle
-        GameObject cube = ab.LoadAsset<GameObject>("monkey_lp");
+        GameObject monkey = ab.LoadAsset<GameObject>("monkey_lp");
+        Instantiate(monkey);
     }
 }
